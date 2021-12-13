74c126e2522524dcf48fc47b505c5ec85fc1564f
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2021-12-13 17:47:05 +0530

    📦 NEW: Add Categories Sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -20,29 +20,16 @@
     <div class="well">
         <h4>Blog Categories</h4>
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-12">
                 <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.col-lg-6 -->
-            <div class="col-lg-6">
-                <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
+                    <?php
+                    $query = "SELECT * FROM categories LIMIT 4";
+                    $select_categories_sidebar = mysqli_query($connection, $query);
+                    while ($row = mysqli_fetch_assoc($select_categories_sidebar)) {
+                        $cat_title = $row['cat_title'];
+                        echo "<li><a href='#'>$cat_title</a></li>";
+                    }
+                    ?>
                 </ul>
             </div>
             <!-- /.col-lg-6 -->
@@ -51,10 +38,6 @@
     </div>
 
     <!-- Side Widget Well -->
-    <div class="well">
-        <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci
-            accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-    </div>
+    <?php include 'widget.php'; ?>
 
 </div>
