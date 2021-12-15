9e643179e67b4dd9ebf52fe4beb49222f5a2a03a
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2021-12-15 11:47:21 +0530

    🐛 FIX: Minor UX Fix

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
             $select_all_posts_query = mysqli_query($connection, $query);
 
             while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                $post_id = $row['post_id'];
                 $post_title = $row['post_title'];
                 $post_author = $row['post_author'];
                 $post_date = $row['post_date'];
@@ -24,24 +25,20 @@
                 $post_content = $row['post_content'];
                 ?>
 
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
-
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#"><?php echo $post_title ?></a>
+                    <a href="post.php?p_id=<?php echo $post_id ?>"><?php echo $post_title ?></a>
                 </h2>
                 <p class="lead">
                     by <a href="index.php"><?php echo $post_author; ?></a>
                 </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php $date = date_create($post_date);
+                    echo date_format($date, "F j, Y"); ?></p>
                 <hr>
                 <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="<?php echo $post_title; ?>">
                 <hr>
                 <p><?php echo $post_content; ?></p>
-                <a class="btn btn-primary" href="#">Read More <span
+                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id ?>">Read More <span
                             class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
