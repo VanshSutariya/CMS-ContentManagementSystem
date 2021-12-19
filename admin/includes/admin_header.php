2c0704650455fdc219b4ebac0a34996cf69b4551
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2021-12-19 09:49:33 +0530

    📦 NEW: Add Logout Logic

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -4,8 +4,10 @@
 <?php include './functions.php' ?>
 
 <?php
-if (isset($_SESSION['user_role'])) {
-    if ($_SESSION['user_role'] == 'subscriber') {
+if (!isset($_SESSION['user_role'])) {
+    header("Location: ../index.php");
+} else if (isset($_SESSION['user_role'])) {
+    if ($_SESSION['user_role'] !== 'admin') {
         header("Location: ../index.php");
     }
 }
