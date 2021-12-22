2fad5535967023fc4958fd0936acd7cc1bdc96b4
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2021-12-22 11:33:53 +0530

    📦 NEW: Extra Element to Dashboard

--- admin/index.php
+++ admin/index.php
@@ -135,6 +135,10 @@
 
                 <div class="row">
                     <?php
+                    $query = "SELECT * FROM posts WHERE post_status = 'published'";
+                    $select_all_published_post = mysqli_query($connection, $query);
+                    $post_published_count = mysqli_num_rows($select_all_published_post);
+
                     $query = "SELECT * FROM posts WHERE post_status = 'draft'";
                     $select_all_draft_post = mysqli_query($connection, $query);
                     $post_draft_count = mysqli_num_rows($select_all_draft_post);
@@ -155,10 +159,10 @@
                             var data = google.visualization.arrayToDataTable([
                                 ['', ''],
                                 <?php
-                                $element_text = ['Posts', 'Draft Posts', 'Comments', 'Pending Comments', 'Users', 'Subscribers', 'Categories'];
-                                $element_count = [$post_count, $post_draft_count, $comment_count, $unapproved_comment_count, $user_count, $subscriber_count, $category_count];
+                                $element_text = ['All Posts', 'Active Posts', 'Draft Posts', 'Comments', 'Pending Comments', 'Users', 'Subscribers', 'Categories'];
+                                $element_count = [$post_count, $post_published_count, $post_draft_count, $comment_count, $unapproved_comment_count, $user_count, $subscriber_count, $category_count];
 
-                                for ($i = 0; $i < 6; $i++) {
+                                for ($i = 0; $i < 8; $i++) {
                                     // echo "['{$element_text[$i]}', {$element_count[$i]}]" . ",";
                                     echo "['{$element_text[$i]}'" . "," . "{$element_count[$i]}],";
                                 }
