ac87e6611688f3dc0a44601b9c18cc44f88e05c4
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2021-12-22 10:43:04 +0530

    👌 IMPROVE: Remove Read More Button

--- index.php
+++ index.php
@@ -53,9 +53,6 @@
                      alt="<?php echo $post_title; ?>">
                 <hr>
                 <p><?php echo $post_content; ?></p>
-                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id ?>">Read More <span
-                            class="glyphicon glyphicon-chevron-right"></span></a>
-
                 <hr>
 
                 <?php
