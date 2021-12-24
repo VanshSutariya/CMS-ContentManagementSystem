91b5f4a0314ac7dcf656a5c871a6f72fd14270b2
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2021-12-24 09:08:57 +0530

    📦 NEW: Add Post Edit Link

--- includes/navigation.php
+++ includes/navigation.php
@@ -2,7 +2,8 @@
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -13,17 +14,32 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <?php
-                $query = "SELECT * FROM categories";
-                $select_all_categories_query = mysqli_query($connection, $query);
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">
+                        More Posts <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <?php
+                        $query = "SELECT * FROM categories";
+                        $select_all_categories_query = mysqli_query($connection, $query);
 
-                while ($row = mysqli_fetch_assoc($select_all_categories_query)) {
-                    $cat_id = $row['cat_id'];
-                    $cat_title = $row['cat_title'];
-                    echo "<li><a href='category.php?category=$cat_id'>$cat_title</a></li>";
+                        while ($row = mysqli_fetch_assoc($select_all_categories_query)) {
+                            $cat_id = $row['cat_id'];
+                            $cat_title = $row['cat_title'];
+                            echo "<li><a href='category.php?category=$cat_id'>$cat_title</a></li>";
+                        }
+                        ?>
+                    </ul>
+                </li>
+
+                <li><a href="admin">Admin</a></li>
+                <?php
+                if (isset($_SESSION['user_role'])) {
+                    if (isset($_GET['p_id'])) {
+                        $the_post_id = $_GET['p_id'];
+                        echo "<li><a href='admin/posts.php?source=edit_post&p_id=$the_post_id'>Edit Post</a></li>";
+                    }
                 }
                 ?>
-                <li><a href="admin">Admin</a></li>
             </ul>
         </div>
         <!-- /.navbar-collapse -->
