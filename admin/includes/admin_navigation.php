63c8b0294e3fcbff880e14df9381a6cc7ce56fde
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2022-01-25 09:46:43 +0530

    📦 NEW: Display Users Online in Navigation

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -11,6 +11,7 @@
     </div>
     <!-- Top Menu Items -->
     <ul class="nav navbar-right top-nav">
+        <li><a href="">Users Online: <?php echo usersOnline(); ?></a></li>
         <li>
             <a href="../index.php"><i class="fa fa-home"></i> HOME SITE</a>
         </li>
