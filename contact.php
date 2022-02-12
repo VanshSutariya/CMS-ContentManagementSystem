382e43170f1e9f9d3e0bef17477585fce9fcfa92
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2022-02-12 14:25:06 +0530

    👌 IMPROVE: Contact Page UI

--- contact.php
+++ contact.php
@@ -25,27 +25,35 @@ if (isset($_POST['submit'])) {
             <div class="container">
                 <div class="row">
                     <div class="col-xs-6 col-xs-offset-3">
-                        <div class="form-wrap">
-                            <h1>Contact</h1>
-                            <form role="form" action="contact.php" method="post" id="login-form" autocomplete="off">
-                                <div class="form-group">
-                                    <label for="email" class="sr-only">Email</label>
-                                    <input type="email" name="email" id="email" class="form-control"
-                                           placeholder="Email">
-                                </div>
-                                <div class="form-group">
-                                    <label for="subject" class="sr-only">Subject</label>
-                                    <input type="subject" name="subject" id="subject" class="form-control"
-                                           placeholder="Subject">
-                                </div>
-                                <div class="form-group">
-                                    <textarea class="form-control" name="body" id="body"></textarea>
-                                </div>
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <div class="text-center">
 
-                                <input type="submit" name="submit" id="btn-login"
-                                       class="btn btn-custom btn-lg btn-block"
-                                       value="Submit">
-                            </form>
+                                    <h2 class="text-center">Contact</h2>
+
+                                    <form role="form" action="contact.php" method="post" id="login-form"
+                                          autocomplete="off">
+                                        <div class="form-group">
+                                            <label for="email" class="sr-only">Email</label>
+                                            <input type="email" name="email" id="email" class="form-control"
+                                                   placeholder="Email">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="subject" class="sr-only">Subject</label>
+                                            <input type="subject" name="subject" id="subject" class="form-control"
+                                                   placeholder="Subject">
+                                        </div>
+                                        <div class="form-group">
+                                            <textarea class="form-control" name="body" id="body" placeholder="Message"></textarea>
+                                        </div>
+
+                                        <input type="submit" name="submit" id="btn-login"
+                                               class="btn btn-lg btn-primary btn-block"
+                                               value="Submit">
+                                    </form>
+
+                                </div>
+                            </div>
                         </div>
                     </div> <!-- /.col-xs-12 -->
                 </div> <!-- /.row -->
