96d89fb413aaf3a18344bd822924df849adb8a27
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2022-02-13 11:14:47 +0530

    📦 NEW: Add Send Emails

--- forgot.php
+++ forgot.php
@@ -24,6 +24,52 @@ if (ifItIsMethod('post')) {
 }
 ?>
 
+<?php
+/*
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host = 'smtp.mailtrap.io';                     //Set the SMTP server to send through
+    $mail->SMTPAuth = true;                                   //Enable SMTP authentication
+    $mail->Username = '82c127223a58e4';                     //SMTP username
+    $mail->Password = '7d1fc27a7c6601';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port = 2525;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('sumeshmajhi@gmail.com', 'Sumesh Majhi');
+    $mail->addAddress('salominag@gmail.net', 'Salomi Nag');     //Add a recipient
+    $mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Love Letter';
+    $mail->Body = 'I <b>Love</b> You.';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+*/?>
+
 <!-- Page Content -->
 <div class="container">
 
