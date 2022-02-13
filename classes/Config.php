3830fe8ef91bc1cfa056d85223c422d56bb83417
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2022-02-13 14:55:59 +0530

    📦 NEW: Add PHPMailer Support (⭐)

---
+++ classes/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+class Config
+{
+    const SMTP_HOST = 'smtp.mailtrap.io';
+    const SMTP_PORT = 2525;
+    const SMTP_USER = '82c127223a58e4';
+    const SMTP_PASSWORD = '7d1fc27a7c6601';
+}
--- forgot.php
+++ forgot.php
@@ -3,6 +3,13 @@
 <?php include "includes/navigation.php"; ?>
 
 <?php
+
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+require 'vendor/autoload.php';
+require 'classes/Config.php';
+
 if (!ifItIsMethod('get') && !isset($_GET['forgot'])) {
     redirect('/php-cms/index');
 }
@@ -18,57 +25,42 @@ if (ifItIsMethod('post')) {
             mysqli_stmt_bind_param($stmt, "s", $email);
             mysqli_stmt_execute($stmt);
             mysqli_stmt_close($stmt);
+
+            /*
+             * CONFIGURE PHPMailer
+             * */
+            $mail = new PHPMailer(true);
+
+            try {
+                //Server settings
+                $mail->isSMTP();
+                $mail->Host = Config::SMTP_HOST;
+                $mail->SMTPAuth = true;
+                $mail->Username = Config::SMTP_USER;
+                $mail->Password = Config::SMTP_PASSWORD;
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->Port = Config::SMTP_PORT;
+
+                //Recipients
+                $mail->setFrom('php@cms.com');
+                $mail->addAddress($email);     //Add a recipient
+
+                //Content
+                $mail->isHTML(true);
+                $mail->Subject = 'Password Reset';
+                $mail->Body = 'Forgot Password';
+
+                $mail->send();
+                echo '<p class="text-center">Message has been sent.</p>';
+            } catch (Exception $e) {
+                echo '<p class="text-center">Message could not be sent.</p>';
+            }
+
         }
     }
 
 }
 ?>
-
-<?php
-/*
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-//Load Composer's autoloader
-require 'vendor/autoload.php';
-
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
-
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host = 'smtp.mailtrap.io';                     //Set the SMTP server to send through
-    $mail->SMTPAuth = true;                                   //Enable SMTP authentication
-    $mail->Username = '82c127223a58e4';                     //SMTP username
-    $mail->Password = '7d1fc27a7c6601';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-    $mail->Port = 2525;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    //Recipients
-    $mail->setFrom('sumeshmajhi@gmail.com', 'Sumesh Majhi');
-    $mail->addAddress('salominag@gmail.net', 'Salomi Nag');     //Add a recipient
-    $mail->addAddress('ellen@example.com');               //Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Love Letter';
-    $mail->Body = 'I <b>Love</b> You.';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
-*/?>
 
 <!-- Page Content -->
 <div class="container">
