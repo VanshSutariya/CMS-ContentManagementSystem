f6d7f7b0200a62a4f94d4e9062c97b0548601ea1
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2022-02-14 19:32:50 +0530

    📦 NEW: Add Toastr for Styling Notifications

--- pusher.php
+++ pusher.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <head>
     <title>Pusher Test</title>
+    <link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet" />
+    <link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.2/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/3.2.1/css/font-awesome.min.css" rel="stylesheet" />
     <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
     <script>
 
@@ -13,7 +16,8 @@
 
         var channel = pusher.subscribe('my-channel');
         channel.bind('my-event', function(data) {
-            alert(JSON.stringify(data));
+            // alert(JSON.stringify(data));
+            toastr.success(JSON.stringify(data));
         });
     </script>
 </head>
@@ -23,4 +27,7 @@
     Try publishing an event to channel <code>my-channel</code>
     with event name <code>my-event</code>.
 </p>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js" ></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 </body>
